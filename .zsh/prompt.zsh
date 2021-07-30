9816d3bd2d32058fad30155dcbe9f7f99e22310d
Lycolia Rizzim <lycolia@lycolia.info>
2021-07-31 02:33:34 +0900

    update prompt

--- .zsh/prompt.zsh
+++ .zsh/prompt.zsh
@@ -1,16 +1,13 @@
 
+setopt prompt_subst
 if [ "$USERNAME" = "root" ]; then CARETCOLOR="red"; else CARETCOLOR="magenta"; fi
 
-GIT_BRANCH=$(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/')
+ZSH_HOME="${HOME}/.zsh"
+source $ZSHHOME/libs/git-prompt.zsh
+
 
 PROMPT="
 %{$fg_bold[cyan]%}%n%{$reset_color%}%{$fg_bold[blue]%}@%m%{$reset_color%}:%{${fg_bold[green]}%}%~%{$reset_color%}
-%(?:%{$fg_bold[green]%}➜:%{$fg_bold[red]%}➜)  %{${reset_color}%}$($GIT_BRANCH)"
+%(?:%{$fg_bold[green]%}➜:%{$fg_bold[red]%}➜)  %{${reset_color}%}"'$(print_git_prompt)'
 
 RPS1="%(?..%{$fg_bold[red]%} [ %? ]%{$reset_color%}) %D - %*"
-
-ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg_bold[magenta]%}(%{$reset_color%}%{$fg_bold[yellow]%}"
-ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%}  "
-ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg_bold[magenta]%})%{$fg_bold[red]%}✗"
-ZSH_THEME_GIT_PROMPT_UNTRACKED="%{$fg_bold[magenta]%})%{$fg[cyan]%} ？"
-ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg_bold[magenta]%})%{$fg_bold[blue]%} ✓"
