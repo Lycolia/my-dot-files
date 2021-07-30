22f23e53d72aad4d04ef354ab29f4133be1143ac
Lycolia Rizzim <lycolia@lycolia.info>
2021-07-31 02:51:55 +0900

    bugfix

--- .zsh/prompt.zsh
+++ .zsh/prompt.zsh
@@ -1,9 +1,29 @@
-
-setopt prompt_subst
 if [ "$USERNAME" = "root" ]; then CARETCOLOR="red"; else CARETCOLOR="magenta"; fi
 
-ZSH_HOME="${HOME}/.zsh"
-source $ZSHHOME/libs/git-prompt.zsh
+ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg_bold[magenta]%}(%{$reset_color%}%{$fg_bold[yellow]%}"
+ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%} "
+
+current_git_branch() {
+  (git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')
+}
+
+parse_git_dirty() {
+  local DIRTY=$(command git status --porcelain | tail -1)
+  if [[ -n $DIRTY ]]; then
+    echo "%{$fg_bold[magenta]%})%{$fg_bold[red]%}✗"
+  else
+    echo "%{$fg_bold[magenta]%})%{$fg_bold[blue]%}✓"
+  fi
+}
+
+print_git_prompt() {
+  if command git branch 2> /dev/null 1> /dev/null; then
+    echo $ZSH_THEME_GIT_PROMPT_PREFIX$(current_git_branch)$(parse_git_dirty)" $ZSH_THEME_GIT_PROMPT_SUFFIX"
+  else
+    echo ""
+  fi
+}
+
 
 
 PROMPT="
