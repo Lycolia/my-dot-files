2c5e2563e1b0f76b8c468442b15b16bf8c999a7c
Lycolia Rizzim <33796432+Lycolia@users.noreply.github.com>
2021-10-08 17:38:11 +0900

    add keybind and refactor for enviroments for platforms
    - Del
    - Home
    - End

--- .zsh/keybind.zsh
+++ .zsh/keybind.zsh
@@ -1,11 +1,25 @@
-# for MSYS2
-## arrow up
-bindkey '^[[A' up-line-or-search
-## arrow down
-bindkey '^[[B' down-line-or-search
+# for common
+## Del
+bindkey '^[[3~' delete-char
 
-# for Ubuntu
-## arrow up
-bindkey "^[OA" up-line-or-search
-## arrow down
-bindkey "^[OB" down-line-or-search
+if [[ $OSTYPE = 'msys' ]]; then
+    # for MSYS2
+    ## arrow up
+    bindkey '^[[A' up-line-or-search
+    ## arrow down
+    bindkey '^[[B' down-line-or-search
+    ## Home
+    bindkey "\e[H" beginning-of-line
+    ## End
+    bindkey "\e[F" end-of-line
+else
+    # for Linux
+    ## arrow up
+    bindkey "^[OA" up-line-or-search
+    ## arrow down
+    bindkey "^[OB" down-line-or-search
+    ## Home
+    bindkey "\e[1~" beginning-of-line
+    ## End
+    bindkey "\e[4~" end-of-line
+fi
